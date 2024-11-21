6fbe908663c2fa37115597b95327dfa6d49aa46d
Callum Mundine <callum@paperplan.co>
2024-11-21 12:29:42 +0100

    Update Remix config for Netlify

---
+++ remix.config.js
@@ -0,0 +1,10 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+  ignoredRouteFiles: ["**/.*"],
+  serverModuleFormat: "esm",
+  serverPlatform: "node",
+  tailwind: true,
+  postcss: true,
+  watchPaths: ["./tailwind.config.ts"],
+  serverDependenciesToBundle: [/^remix-utils.*/],
+};
